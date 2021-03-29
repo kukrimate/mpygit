c6d1b785dd5bdc9585ce3bbd546505c73bda8c61
Mate Kukri <kukri.mate@gmail.com>
2021-03-29 13:37:57 +0100

    Add license

--- mpygit.py
+++ mpygit.py
@@ -1,11 +1,19 @@
 #
 # Read only, pure Python git implementation
+# Copyright (C) 2021  Mate Kukri
 #
-# This is really outside the scope of the WAD2 project, but I decided to write
-# a replacement for pygit2 as it was not flexible enough for our requirements
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Author: Mate Kukri
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import binascii
 import configparser
