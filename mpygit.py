0e959dadb4b7179753ff95988884ce145726e032
Mate Kukri <kukri.mate@gmail.com>
2021-03-29 17:29:23 +0100

    Add utility class with `diff_commits`

--- mpygit.py
+++ mpygit.py
@@ -1,5 +1,6 @@
 #
-# Read only, pure Python git implementation
+# Part of mpygit - mpygit.py - Read only, pure Python git implementation
+#
 # Copyright (C) 2021  Mate Kukri
 #
 # This program is free software: you can redistribute it and/or modify
